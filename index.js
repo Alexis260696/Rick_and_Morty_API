528db69cb38fc0306570c3041e7f513000616bff
Derian Alexis Flores Perez <125107629+Alexis260696@users.noreply.github.com>
2024-02-20 12:25:42 -0600

    Actualizar index.js

--- index.js
+++ index.js
@@ -29,6 +29,17 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
 
 
+app.get('/character/:id', (req, res) => {
+    const characterId = parseInt(req.params.id);
+    const character = req.characters.find(char => char.id === characterId);
+    if (character) {
+        res.send(character);
+    } else {
+        res.status(404).send('Character not found');
+    }
+});
+
+
 app.get('/resetLocal', (req, res) => {
     let getPage = (url, getNextPage, db = []) => {
         axios.get(url)
